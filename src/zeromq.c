8b3292caa929b3adf0d4a267125b7acf1b1e9e58
Victor Perron <victor@iso3103.net>
2013-06-12 11:00:29 +0200

    Retab zeromq.c

--- src/zeromq.c
+++ src/zeromq.c
@@ -24,55 +24,55 @@
 
 void zeromq_send_data(void *socket, char *identity, uint8_t *data, int size)
 {
-	assert(socket);
-	assert(data);
+  assert(socket);
+  assert(data);
 
-	zmsg_t *msg = zmsg_new();
+  zmsg_t *msg = zmsg_new();
 
-	zmsg_addstr (msg, "%s", identity);
-	zframe_t *frame = zframe_new (data, size);
-	zmsg_add (msg, frame);
+  zmsg_addstr (msg, "%s", identity);
+  zframe_t *frame = zframe_new (data, size);
+  zmsg_add (msg, frame);
 
-	zmsg_send (&msg, socket);
+  zmsg_send (&msg, socket);
 }
 
 void *zeromq_create_socket (zctx_t *context, const char *endpoint, int type,
-		const char *topic, bool connect, int linger, int hwm)
+    const char *topic, bool connect, int linger, int hwm)
 {
 
-	void *socket = NULL;
+  void *socket = NULL;
 
-	assert(context);
-	assert(endpoint);
+  assert(context);
+  assert(endpoint);
 
-	if (endpoint[0] == 0) {
-		errorLog("Trying to open NULL output socket !");
-		return NULL;
-	}
+  if (endpoint[0] == 0) {
+    errorLog("Trying to open NULL output socket !");
+    return NULL;
+  }
 
-	socket = zsocket_new (context, type);
-	if (hwm != -1) {
-		zsocket_set_sndhwm (socket, hwm);
-		zsocket_set_rcvhwm (socket, hwm);
-	}
+  socket = zsocket_new (context, type);
+  if (hwm != -1) {
+    zsocket_set_sndhwm (socket, hwm);
+    zsocket_set_rcvhwm (socket, hwm);
+  }
 
-	if (linger != -1) {
-		zsocket_set_linger (socket, linger);
-	} else { // Set linger to 0 as default, safety measure
-		zsocket_set_linger (socket, 0);
-	}
+  if (linger != -1) {
+    zsocket_set_linger (socket, linger);
+  } else { // Set linger to 0 as default, safety measure
+    zsocket_set_linger (socket, 0);
+  }
 
   // Allow for exponential backoff: up to 5 minutes and 1s minimum
   zsocket_set_reconnect_ivl(socket, 1 * 1000);
   zsocket_set_reconnect_ivl_max(socket, 5 * 60 * 1000);
 
-	if (type == ZMQ_SUB)
-		zsocket_set_subscribe (socket, (char*)(topic == NULL ? "" : topic));
+  if (type == ZMQ_SUB)
+    zsocket_set_subscribe (socket, (char*)(topic == NULL ? "" : topic));
 
-	if (connect) {
-		zsocket_connect (socket, "%s", endpoint);
-	} else {
-		zsocket_bind (socket, "%s", endpoint);
-	}
-	return socket;
+  if (connect) {
+    zsocket_connect (socket, "%s", endpoint);
+  } else {
+    zsocket_bind (socket, "%s", endpoint);
+  }
+  return socket;
 }
